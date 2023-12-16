e64ab25a82f7444117cfed8e7ee7b10a6296e5bd
Fábio Ribeiro dos Santos Quispe <fabio.santos@ambevtech.com.br>
2023-12-16 00:16:15 -0300

    Desafio Felipão

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('Olá mundão!!!!')
+let nome = "Fábio Quispe";
+let xp = 19244;
+let nivel = "";
+
+const niveis = [
+  { nome: "Ferro", limite: 1000 },
+  { nome: "Bronze", limite: 2000 },
+  { nome: "Prata", limite: 5000 },
+  { nome: "Ouro", limite: 7000 },
+  { nome: "Platina", limite: 8000 },
+  { nome: "Ascendente", limite: 9000 },
+  { nome: "Imortal", limite: 10000 },
+  { nome: "Radiante", limite: Infinity }
+];
+
+for (const { nome: nivelNome, limite: nivelLimite } of niveis) {
+  if (xp <= nivelLimite) {
+    nivel = nivelNome;
+    break;
+  }
+}
+
+console.log(`O Herói de nome **{${nome}}** está no nível de **{${nivel}}**`);
